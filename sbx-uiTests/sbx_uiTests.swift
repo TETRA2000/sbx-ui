7cfee0b163e36fbcb275d88a0ce3888f5ac33789
Takahiko Inayama <sagittariusm25@gmail.com>
2026-04-04 17:15:11 +0900

    Init Xcode project

---
+++ sbx-uiTests/sbx_uiTests.swift
@@ -0,0 +1,19 @@
+//
+//  sbx_uiTests.swift
+//  sbx-uiTests
+//
+//  Created by Takahiko Inayama on 2026/4/4.
+//
+
+import Testing
+@testable import sbx_ui
+
+struct sbx_uiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
